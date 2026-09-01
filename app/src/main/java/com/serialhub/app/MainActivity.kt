5eca641485579a8bbe93957f4c38d8f31ba4f719
d21419205-crypto <d21419205@gmail.com>
2026-09-01 15:16:04 +0500

    Create MainActivity.kt

---
+++ app/src/main/java/com/serialhub/app/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.serialhub.app
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val webView = WebView(this)
+
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.loadWithOverviewMode = true
+        webView.settings.useWideViewPort = true
+
+        webView.webViewClient = WebViewClient()
+
+        webView.loadUrl("https://serialhub12.blogspot.com/")
+
+        setContentView(webView)
+    }
+}
