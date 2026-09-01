4ba9460f6e78f2265f3cde112cafcfb2a2f65cd9
d21419205-crypto <d21419205@gmail.com>
2026-09-01 16:45:57 +0500

    Update MainActivity.kt

--- app/src/main/java/com/serialhub/app/MainActivity.kt
+++ app/src/main/java/com/serialhub/app/MainActivity.kt
@@ -1,26 +1,95 @@
 package com.serialhub.app
 
-import android.app.Activity
+import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+    private var customView: View? = null
+    private var customViewContainer: FrameLayout? = null
+    private var originalOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val webView = WebView(this)
+        originalOrientation = requestedOrientation
 
-        webView.settings.javaScriptEnabled = true
-        webView.settings.domStorageEnabled = true
-        webView.settings.loadWithOverviewMode = true
-        webView.settings.useWideViewPort = true
+        webView = WebView(this)
+
+        webView.settings.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            mediaPlaybackRequiresUserGesture = false
+            loadWithOverviewMode = true
+            useWideViewPort = true
+        }
 
         webView.webViewClient = WebViewClient()
 
-        webView.loadUrl("https://serialhub12.blogspot.com/")
+        webView.webChromeClient = object : WebChromeClient() {
+
+            override fun onShowCustomView(
+                view: View?,
+                callback: CustomViewCallback?
+            ) {
+                if (customView != null) {
+                    callback?.onCustomViewHidden()
+                    return
+                }
+
+                customView = view
+                customViewContainer = FrameLayout(this@MainActivity)
+
+                customViewContainer?.addView(
+                    view,
+                    FrameLayout.LayoutParams(
+                        FrameLayout.LayoutParams.MATCH_PARENT,
+                        FrameLayout.LayoutParams.MATCH_PARENT
+                    )
+                )
+
+                setContentView(customViewContainer)
+
+                requestedOrientation =
+                    ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+            }
+
+            override fun onHideCustomView() {
+                customViewContainer?.removeAllViews()
+                customViewContainer = null
+                customView = null
+
+                setContentView(webView)
+
+                requestedOrientation =
+                    ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+            }
+        }
 
         setContentView(webView)
+
+        webView.loadUrl("https://serialhub12.blogspot.com/")
+    }
+
+    override fun onBackPressed() {
+        if (customView != null) {
+            webView.webChromeClient?.onHideCustomView()
+        } else if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onDestroy() {
+        webView.destroy()
+        super.onDestroy()
     }
 }
